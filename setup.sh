46692b43ac53f1dac19f27124b5616e03ece98a6
ChrisSteinbach <github@cfs.mm.st>
2017-10-12 11:20:28 +0200

    Setup vim plugins

--- setup.sh
+++ setup.sh
@@ -5,7 +5,21 @@ install() {
    sudo apt install -y ${pkg}
 }
 
+copy_if_missing() {
+  if ! test -f $1 ; then
+      echo "Copying $1 to home directory"
+      cp $1 ${HOME}/.vimrc
+  fi
+}
+
 install vim
+copy_if_missing .vimrc
+if ! test -f ~/.vim/bundle/Vundle.vim ; then
+    echo "Installing vundle"
+    git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+    vim +PluginInstall +qall
+fi
+
 install openssh-server
 
 git config core.editor vim
